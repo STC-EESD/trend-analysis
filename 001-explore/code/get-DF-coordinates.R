bbd7477fcd0483f6c355c66c3b7f28b2a92220f6
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-05-15 12:11:37 -0400

    added: get-DF-coordinates.R

---
+++ 001-explore/code/get-DF-coordinates.R
@@ -0,0 +1,56 @@
+
+get.DF.coordinates <- function(
+    GDB.SpatialData     = NULL,
+    parquet.SpatialData = "SF-SpatialData.parquet"
+    ){
+
+    thisFunctionName <- "get.DF.coordinates";
+
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###");
+    cat(paste0("\n# ",thisFunctionName,"() starts.\n"));
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    SF.SpatialData <- sf::st_read(GDB.SpatialData);
+    SF.SpatialData <- cbind(SF.SpatialData,sf::st_coordinates(SF.SpatialData));
+    colnames(SF.SpatialData) <- gsub(
+        x           = colnames(SF.SpatialData),
+        pattern     = "pointid",
+        replacement = "pointID"
+        );
+    colnames(SF.SpatialData) <- gsub(
+        x           = colnames(SF.SpatialData),
+        pattern     = "^X$",
+        replacement = "x"
+        );
+    colnames(SF.SpatialData) <- gsub(
+        x           = colnames(SF.SpatialData),
+        pattern     = "^Y$",
+        replacement = "y"
+        );
+    arrow::write_parquet(x = SF.SpatialData, sink = parquet.SpatialData);
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    x.coords <- sort(unique(unlist(sf::st_drop_geometry(SF.SpatialData[,'x']))));
+    y.coords <- sort(unique(unlist(sf::st_drop_geometry(SF.SpatialData[,'y']))));
+
+    get.coordinate.indexes <- function(coordinates = NULL) {
+        which.x <- which( coordinates[1] == x.coords );
+        which.y <- which( coordinates[2] == y.coords );
+        if (length(which.x) == 0) { which.x <- NA };
+        if (length(which.y) == 0) { which.y <- NA };
+        output.vector <- c(which.x,which.y);
+        return(output.vector);
+        }
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    list.output <- list(
+        SF.SpatialData         = SF.SpatialData,
+        get.coordinate.indexes = get.coordinate.indexes
+        );
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    cat(paste0("\n# ",thisFunctionName,"() exits."));
+    cat("\n### ~~~~~~~~~~~~~~~~~~~~ ###\n");
+    return( list.output );
+
+    }
